facb06f9a0c28e91594be616bc0f908e8c2a836d
Vicente Lobos <vicentelobosg@gmail.com>
2022-11-01 21:30:15 -0300

    refactor: custom hook que me devuelve imagenes y el estado del fetch

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,22 +1,17 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs"
+import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
 
 export const GifGrid = ({category}) => {
-    const [gifs, setGifs] = useState([]);
-    
-    const getImages = async () => {
-        const newGifs = await getGifs(category);
-        setGifs(newGifs)
-    }
-
-    useEffect(() => {
-        getImages();
-    }, []);
+    const {gifs, isLoading} = useFetchGifs(category);
+    console.log({ gifs, isLoading})
 
     return (
         <div>   
             <h3>{ category }</h3>
+
+            {
+                isLoading && ( <h2> Cargando... </h2> )
+            }
             
             <div className="card-grid">
                 {
